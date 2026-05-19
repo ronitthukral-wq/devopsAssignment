cfc3256d2b2425171e493697023f099da87afd0b
ronitthukral-wq <ronit.thukral@lng-consultancy.com>
2026-05-19 12:09:25 +0530

    second commit

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "rg-terraform-management"
-    storage_account_name = "YOUR_ACTUAL_STORAGE_ACCOUNT_NAME" 
+    storage_account_name = "storagergfirst13" 
     container_name       = "tfstate"                         
     key                  = "terraform.tfstate"
   }
