2828f586eee05e8e01069ddb5a795ac044134b9e
ronitthukral-wq <ronit.thukral@lng-consultancy.com>
2026-05-19 12:48:05 +0530

    added storage account in reg demo resource group

--- main.tf
+++ main.tf
@@ -11,3 +11,13 @@ resource "azurerm_virtual_network" "pipeline_vnet" {
   location            = azurerm_resource_group.pipeline_rg.location
   resource_group_name = azurerm_resource_group.pipeline_rg.name
 }
+
+
+# Create a Storage Account inside the same Resource Group
+resource "azurerm_storage_account" "pipeline_storage" {
+  name                     = "storagepipelinedemo22"   
+  resource_group_name      = azurerm_resource_group.pipeline_rg.name
+  location                 = azurerm_resource_group.pipeline_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"                   
+}
